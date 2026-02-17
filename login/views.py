28b2ae80bd14081c588bbffb600571a9ceb33d55
alanbabu43 <alanbabu505@gmail.com>
2026-02-17 12:02:59 +0530

    first commit

---
+++ login/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login, logout
+from django.contrib.auth.hashers import make_password
+# Create your views here.
